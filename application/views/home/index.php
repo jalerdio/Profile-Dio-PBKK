272040daaedab6c12cec9b05276d1aded794b58d
Jaler Dio Fadhilah <37465606+jalerdio@users.noreply.github.com>
2020-03-19 21:47:56 +0700

    rearrange

---
+++ application/views/home/index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <title>Jalerdio Profile</title>
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-md  fixed-top maine-menu">
+    <div class="container">
+      <button class="navbar-toggler ml-auto" data-target="#my-nav" onclick="myFunction(this)" data-toggle="collapse"> <span class="bar1"></span> <span class="bar2"></span> <span class="bar3"></span> </button>
+      <div id="my-nav" class="collapse navbar-collapse">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item active"> <a class="nav-link" href="#">Home</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="#about" tabindex="-1" aria-disabled="true">About</a></li>
+          <li class="nav-item"> <a class="nav-link" href="#activity" tabindex="-1" aria-disabled="true">Experience</a></li>
+          <li class="nav-item"> <a class="nav-link" href="#sosmed" tabindex="-1" aria-disabled="true">Social Media</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="container-fluid fh5co-home-banner">
+    <div class="card"> <img class="card-img" src="assets/img/foto.jpg">
+      <div class="card-img-overlay">
+        <div class="center-text">
+         <!--<h2 class="card-title" >Biodata</h2>-->
+         <img class="logotype" src="assets/img/jalerdio_logotype.png">
+     
+            <svg width="201" height="51" viewBox="0 0 201 51">
+              <defs>
+                <style>
+                .cls-1 {
+                  fill: none;
+                  stroke-width: 1px;
+                  stroke: url(#linear-gradient);
+                }
+              </style>
+              <linearGradient id="linear-gradient" x1="140.508" y1="50.5" x2="60.492" y2="0.5" gradientUnits="userSpaceOnUse">
+                <stop offset="0" stop-color="#e90e65"/>
+                <stop offset="1" stop-color="#fff"/>
+              </linearGradient>
+            </defs>
+            
+          </svg>
+       
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid fh5co-two-img">
+    <div class="row">
+      <div class="col-sm-6 pr-0 pl-0" style="background-color: black">
+      <img src="assets/img/foto_saya.png" alt="foto saya" height="100%" width="100%" style="display: block; margin: auto;">
+      </div>
+      <div class="col-sm-6 pr-0 pl-0" style="background-color: black">
+        
+          <div class="content" style="color: white; font-family:sans-serif" id="about">
+            <h3 style="font-size:8vw;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;About</h3>
+            <hr color="white">
+            <p style="font-size:1.5vw;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: Jaler Dio Fadhilah</p>
+            <p style="font-size:1.5vw;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Address&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: Keputih Tegal Timur II No.34</p>
+            <p style="font-size:1.5vw;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;University&nbsp;&nbsp;: Institut Teknologi Sepuluh Nopember</p>
+            <p style="font-size:1.5vw;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: jalerdio@gmail.com</p>
+            <br>
+            <p style="font-size:1.5vw;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A part-time explorer who has a concern at Front-End Development fields. Currently studying at Informatics Department.</p>        
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid fh5co-two-img">
+    <div class="row">
+      <div class="col-sm-6 pr-0 pl-0" style="background-color: black">
+      <img src="assets/img/work.jpg" alt="foto saya" height="100%" width="100%" style="display: block; margin: auto;">
+      </div>
+      <div class="col-sm-6 pr-0 pl-0" style="background-color: black">
+        
+          <div class="content" style="color: white; font-family:sans-serif" id="activity">
+            <h3 style="font-size:8vw;">&nbsp;&nbsp;&nbsp;Experience</h3>
+            <p>
+              <hr color="white">
+              <li style="font-size:1.5vw;">Internship at Microsoft Indonesia at Product Department</li>
+              <li style="font-size:1.5vw;">Chief of Creative Media at Schematics ITS</li>
+              <li style="font-size:1.5vw;">Internship at PT Bukit Makmur Mandiri Utama at IT Department</li>
+              <li style="font-size:1.5vw;">Official Division of Bayucaraka UAV Research Team ITS Surabaya</li>
+              <li style="font-size:1.5vw;">Staff of Information Media at HMTC ITS</li>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid fh5co-insta-feed activity" id="sosmed">
+    <div class="container recent">
+      <h2 class="text-center d-block">Find me on social media</h2>
+      <div class="row social-links">
+        <ul class="nav mx-auto">
+          <li class="nav-item"> <a class="nav-link" href="http://github.com/jalerdio"><img src="assets/img/Github1.png" alt=""></a> </li>
+          <li class="nav-item"> <a class="nav-link" href="https://www.instagram.com/jalerdio/"><img src="assets/img/ig.png" alt=""></a> </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <footer class="container-fluid p-0 pr-0">
+    <div class="copy pt-4 pb-4">
+      <p><a href="https://www.instagram.com/jalerdio/" target="_blank"> &copy; 2020 jalerdio</a></p>
+    </div>
+  </footer>
+
+  <!-- jQuery first, then Popper.js, then Bootstrap JS --> 
+  <script src="assets/js/jquery-3.3.1.slim.min.js"></script> 
+  <script src="assets/js/popper.min.js" ></script> 
+  <script src="assets/js/bootstrap.min.js"></script> 
+  <script src="assets/js/owl.carousel.min.js"></script> 
+  <script src="assets/js/main.js"></script>
+
+</body>
+</html>
